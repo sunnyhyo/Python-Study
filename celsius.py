f5008b0d9836cb14ffcdd0649ce4dd1c311a36ad
sunnyhyo <sunnyhyo77@gmail.com>
2018-07-01 22:46:55 +0900

    u

---
+++ celsius.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 11:08:04 2018
+
+@author: HS
+"""
+
+ftemp =77
+celsius = (ftemp -32)/1.8
+celsius
+
+
+ftemp =76
+num = ftemp -32
+celsius = num/1.8
+celsius
